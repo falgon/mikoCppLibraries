c8b6c190cece6f1c215c8020a9f9ea954597c1be
falgon <falgon53@yahoo.co.jp>
2012-01-18 22:04:03 +0900

    libs

---
+++ miko/algorithm/casting.hpp
@@ -0,0 +1,92 @@
+namespace miko{
+template<class from>
+class Static_Cast{
+	from var;
+public:
+	Static_Cast(from f):var(f){}
+	template<class T>
+	operator T()const
+	{
+		return static_cast<T>(var);
+	}
+};
+template<class from>
+Static_Cast<from> st_cast(from v)
+{
+	return Static_Cast<from>(v);
+}
+
+template<class from>
+class Dynamic_Cast{
+	from var;
+public:
+	Dynamic_Cast(from f):var(f){}
+	template<class T>
+	operator T()const
+	{
+		return dynamic_cast<T>(var);
+	}
+};
+template<class from>
+Dynamic_Cast<from> dy_cast(from v)
+{
+	return Dynamic_Cast<from>(v);
+}
+
+template<class from>
+class Const_Cast{
+	from var;
+public:
+	Const_Cast(from f):var(f){}
+	template<class T>
+	operator T()const
+	{
+		return const_cast<T>(var);
+	}
+};
+template<class from>
+Const_Cast<from> co_cast(from v)
+{
+	return Const_Cast<from>(v);
+}
+
+template<class from>
+class Reinterpret_Cast{
+	from var;
+public:
+	Reinterpret_Cast(from f):var(f){}
+	template<class T>
+	operator T()const
+	{
+		return reinterpret_cast<T>(var);
+	}
+};
+template<class from>
+Reinterpret_Cast<from> re_cast(from v)
+{
+	return Reinterpret_Cast<from>(v);
+}
+
+#include<sstream>
+
+template<class from>
+class Lexical_Cast{
+	from var;
+public:
+	Lexical_Cast(from f):var(f){}
+	template<class T>
+	operator T()const
+	{
+		T result;
+		std::stringstream ss;
+		ss<<var;
+		ss>>result;
+		return result;
+	}
+};
+template<class from>
+Lexical_Cast<from> le_cast(from v)
+{
+	return Lexical_Cast<from>(v);
+}
+}
---
+++ miko/glut/first_glut.hpp
@@ -0,0 +1,39 @@
+#include<GL/glut.h>
+namespace miko{
+struct gluting{
+	gluting(const int MODE,const int MODE1,const int MODE2)
+	{
+		glutInitDisplayMode (MODE|MODE1|MODE2);
+	}
+
+	void my_Init()
+	{
+		GLfloat light_position[]={100.0,100.0,100.0,1.0};
+		glLightfv(GL_LIGHT0,GL_POSITION,light_position);
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+		glEnable(GL_DEPTH_TEST);
+	}
+
+	static void disp()
+	{
+		glClear (GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+		glLoadIdentity();
+		gluLookAt(40.0,10.0,20.0,0.0,0.0,0.0,0.0,1.0,0.0);
+		glutSolidCube(2.0);
+		glFlush();
+	}
+
+	static void Reshape(const int w,const int h)
+	{
+		glViewport(0,0,w,h);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluPerspective(10.0,
+				static_cast<GLfloat>(w)/static_cast<GLfloat>(h),
+				1.0,
+				100.0);
+		glMatrixMode(GL_MODELVIEW);
+	}
+};
+}
