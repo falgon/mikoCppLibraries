fbd0ba1f5998d034da3f8b849c1e0e77abb9a7f9
falgon <falgon53@yahoo.co.jp>
2012-03-25 12:51:02 +0900

    added str_replace

--- miko/utility.hpp
+++ miko/utility.hpp
@@ -1,4 +1,5 @@
 #ifndef INCLUDED_UTILITY
 #define INCLUDED_UTILITY
 #include<miko/utility/equal_range_erase.hpp>
+#include<miko/utility/string_replace.hpp>
 #endif
---
+++ miko/utility/string_replace.hpp
@@ -0,0 +1,17 @@
+#include<string>
+#include<cstring>
+
+namespace miko{
+std::string& str_replace(std::string& str,const char* src,const char* rhs)
+{
+	if(str.empty()||!src||!*src||!rhs||!strcmp(src,rhs))
+		return str;
+	const size_t src_len=strlen(src);
+	const size_t rhs_len=strlen(rhs);
+	for(std::string::size_type i=0;
+			(i=str.find(src,i,src_len))!=std::string::npos;
+			i+=rhs_len)
+		str.replace(i,src_len,rhs,rhs_len);
+	return str;
+}
+}
