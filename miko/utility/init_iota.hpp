95af744ea32bdb09e123e2649273ef82b0b66c03
falgon <falgon53@yahoo.co.jp>
2012-03-28 19:18:42 +0900

    名前を

--- miko/utility/init_iota.hpp
+++ miko/utility/init_iota.hpp
@@ -3,4 +3,5 @@
 #include<boost/preprocessor/repetition/repeat_from_to.hpp>
 #define INIT_IOTA_T(z,x,text) x,
 #define INIT_IOTA(x,y) BOOST_PP_REPEAT_FROM_TO(x,y,INIT_IOTA_T,_)
+#define IIOTA INIT_IOTA
 #endif
